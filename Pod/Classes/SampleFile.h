1090d6c192de37ff17fac78967ba734018cec9ba
Siddarth Chaturvedi <siddarth49.pilani@gmail.com>
2014-07-30 13:04:30 +0530

    restructuring.

---
+++ Pod/Classes/SampleFile.h
@@ -0,0 +1,13 @@
+//
+//  SampleFile.h
+//  Pods
+//
+//  Created by siddarth chaturvedi on 30/07/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SampleFile : NSObject
+
+@end
---
+++ Pod/Classes/SampleFile.m
@@ -0,0 +1,13 @@
+//
+//  SampleFile.m
+//  Pods
+//
+//  Created by siddarth chaturvedi on 30/07/14.
+//
+//
+
+#import "SampleFile.h"
+
+@implementation SampleFile
+
+@end
