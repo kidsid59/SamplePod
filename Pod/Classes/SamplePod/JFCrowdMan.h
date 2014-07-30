fdff0ab49fdd29563694bf844ce6512fbaea4073
Siddarth Chaturvedi <siddarth49.pilani@gmail.com>
2014-07-30 14:28:42 +0530

    New files are committed.

---
+++ Pod/Classes/SamplePod/JFCrowdMan.h
@@ -0,0 +1,13 @@
+//
+//  JFCrowdMan.h
+//  Pods
+//
+//  Created by siddarth chaturvedi on 30/07/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JFCrowdMan : NSObject
+
+@end
---
+++ Pod/Classes/SamplePod/JFCrowdMan.m
@@ -0,0 +1,13 @@
+//
+//  JFCrowdMan.m
+//  Pods
+//
+//  Created by siddarth chaturvedi on 30/07/14.
+//
+//
+
+#import "JFCrowdMan.h"
+
+@implementation JFCrowdMan
+
+@end
